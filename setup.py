a44260268834a6053ae97a8fee72111e73860da0
Xiuming Chen <cc@cxm.cc>
2017-05-26 00:46:37 -0700

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires = [
+    'boto3',
+    'click',
+    'configparser',
+    'docker-py',
+    'dockerpty',
+    'jsonpath',
+    'tabulate',
+    'humanize',
+    'pytz',
+]
+
+setup(
+    name='ecsctl',
+    version='20170525',
+    description='kubectl-style command line client for AWS ECS.',
+    author='Xiuming Chen',
+    author_email='cc@cxm.cc',
+    url='https://github.com/cxmcc/ecsctl',
+    packages=['ecsctl'],
+    entry_points={'console_scripts': ['ecsctl = ecsctl.__main__:main']},
+    install_requires=install_requires,
+)
