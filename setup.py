3bde0ac3883c3c23ab9a70b7f22be007d8559a37
Anton Priadko <anton@verbit.ai>
2019-04-25 17:16:43 +0300

    version upd

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='ecsctl',
-    version='20190425-2',
+    version='20190425-3',
     scripts=['ecscli'],
     description='kubectl-style command line client for AWS ECS.',
     long_description=long_description,
